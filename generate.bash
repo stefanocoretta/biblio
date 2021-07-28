a55e6a269f02a6f0324681c68debedf07dd3aeae
stefanocoretta <stefano.coretta@gmail.com>
2021-07-28 18:04:24 +0200

    ♻️ - restyle

--- generate.bash
+++ generate.bash
@@ -5,4 +5,5 @@
 pandoc /Users/ste/texmf/bibtex/bib/linguistics.bib -s --citeproc \
     --csl "/Users/ste/.local/share/pandoc/csl/unified-style-sheet-for-linguistics.csl" \
     --metadata title="Linguistic bibliography" \
+    -H extra.html \
     -o index.html
