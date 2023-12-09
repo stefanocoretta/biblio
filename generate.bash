17667845c7b81efbc6839c6794b888993020651c
stefanocoretta <stefano.coretta@gmail.com>
2023-12-09 10:29:34 +0000

    add bib file

--- generate.bash
+++ generate.bash
@@ -2,7 +2,9 @@
 #
 # Generate bibliography
 
-pandoc /Users/ste/texmf/bibtex/bib/linguistics.bib -s --citeproc \
+cp /Users/ste/texmf/bibtex/bib/linguistics.bib .
+
+pandoc linguistics.bib -s --citeproc \
     --csl "/Users/ste/.local/share/pandoc/csl/unified-style-sheet-for-linguistics.csl" \
     --metadata title="Linguistic bibliography" \
     -H extra.html \
