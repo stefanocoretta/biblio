238ab4c8daea362c7d120358f500823f8885e5d0
stefanocoretta <stefano.coretta@gmail.com>
2024-08-19 13:46:11 +0100

    use quarto citations for full bib

--- generate.bash
+++ generate.bash
@@ -4,11 +4,11 @@
 
 cp /Users/ste/texmf/bibtex/bib/linguistics.bib .
 
-pandoc linguistics.bib -s --citeproc \
-    --csl "/Users/ste/.local/share/pandoc/csl/unified-style-sheet-for-linguistics.csl" \
-    --metadata title="" \
-    -H extra.html \
-    -o biblio-plain.html
+# pandoc linguistics.bib -s --citeproc \
+#     --csl "/Users/ste/.local/share/pandoc/csl/unified-style-sheet-for-linguistics.csl" \
+#     --metadata title=" " \
+#     --template=minimal.html \
+#     -o biblio-plain.html
 
 # git add .
 # git commit -m "edit bib"
